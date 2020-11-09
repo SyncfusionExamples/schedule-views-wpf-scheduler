f343983109badc18afc322d407ac6168b5e64dc8
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-09 11:34:26 +0530

    WPF-55565 - Sample and screenshot added.

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<SfScheduler>
+    {
+        SfScheduler scheduler;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            scheduler = this.AssociatedObject;
+            this.AssociatedObject.CellTapped += Schedule_CellTapped;
+        }
+        private void Schedule_CellTapped(object sender, CellTappedEventArgs e)
+        {
+            this.AssociatedObject.ViewType = SchedulerViewType.Day;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.CellTapped += Schedule_CellTapped;
+            this.scheduler = null;
+        }
+    }
+}
+
